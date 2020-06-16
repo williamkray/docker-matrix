afd307be83dd49c60e8aef141a2aa63a3d68f306
William Kray <github@williamkray.com>
2020-06-16 13:52:58 -0700

    source host info for register script

--- register.sh
+++ register.sh
@@ -4,4 +4,6 @@
 ## users without having to enable registration
 ## on the synapse server
 
+source host.conf
+
 docker-compose exec synapse register_new_matrix_user -c /data/homeserver.yaml https://$HOSTNAME
