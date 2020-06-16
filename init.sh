7b102eb612bb8cb675e94be7ad4393e254bd47e3
William Kray <github@williamkray.com>
2020-06-16 10:01:24 -0700

    include site host routing rules

--- init.sh
+++ init.sh
@@ -49,9 +49,10 @@ echo "updating PostgreSQL password in docker-compose.yml and init script"
 sed -i "s/REPLACE_WITH_POSTGRES_PW/${POSTGRES_PW}/g" storage/postgresql/init-db.sh
 sed -i "s/REPLACE_WITH_POSTGRES_ROOT_PW/${POSTGRES_ROOT_PW}/g" docker-compose.yml
 
-echo "updating synapse docker labels in docker-compose.yml"
+echo "updating docker labels in docker-compose.yml"
 sed -i "s/REPLACE_WITH_MATRIX_HOST/${MATRIX_HOST}/g" docker-compose.yml
 sed -i "s/REPLACE_WITH_HOSTNAME/${HOSTNAME}/g" docker-compose.yml
+sed -i "s/REPLACE_WITH_SITE_HOST/${SITE_HOST}/g" docker-compose.yml
 
 echo "starting with base Riot config file"
 mkdir -p storage/riot/data
